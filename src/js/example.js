3bf43f138566214d2be459e6f69bf9e09447d0a4
Cody Wilson <cody@codifynw.com>
2021-06-06 12:34:44 -0700

    remove prod runtime bundle, start testing SRC

--- src/js/example.js
+++ src/js/example.js
@@ -1,3 +1,7 @@
 export const example = () =>
   `Sensible webpack 5 boilerplate using Babel and PostCSS with a hot dev server 
   and an optimized production build.`;
+
+console.log("above SRC call in example.js");
+console.log("SRC: ", window.SRC);
+console.log("testing");
