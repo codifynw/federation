d58564f736306d77158125fe57b173cc81d02085
Cody Wilson <cody@codifynw.com>
2021-09-17 15:41:59 -0700

    start over

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// Test import of a JavaScript module
-import { example } from "@/js/example";
-
-const heading = document.createElement("h1");
-heading.textContent = example();
-
-const app = document.querySelector("#root");
-app.append(heading);
